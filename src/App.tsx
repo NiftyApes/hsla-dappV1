5a57199b2987c39f3377b47431a7feba5649f964
Kevin Seagraves <kevinseagraves@Kevins-MacBook-Pro-2.local>
2022-04-04 12:23:45 -0600

    add additional eth context stuf

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-// import { EthComponentsSettingsContext, IEthComponentsSettings } from 'eth-components/models';
+import { EthComponentsSettingsContext, IEthComponentsSettings } from 'eth-components/models';
 import { EthersAppContext } from 'eth-hooks/context';
 
-// import { ContractsAppContext } from './config/contractContext';
+import { ContractsAppContext } from './config/contractContext';
 
 import theme from './theme';
 import Marketing from 'routes/Marketing';
@@ -21,21 +21,34 @@ const queryClient = new QueryClient({
   },
 });
 
+const BLOCKNATIVE_DAPPID = '0b58206a-f3c0-4701-a62f-73c7243e8c77';
+
+// create eth components context for options and API keys
+const ethComponentsSettings: IEthComponentsSettings = {
+  apiKeys: {
+    BlocknativeDappId: BLOCKNATIVE_DAPPID,
+  },
+};
+
 const App: React.FC = () => (
-  <EthersAppContext>
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Suspense fallback={<LoadingIndicator fullScreen />}>
-            <Routes>
-              {Marketing}
-              {Borrowers}
-            </Routes>
-          </Suspense>
-        </Router>
-      </QueryClientProvider>
-    </ChakraProvider>
-  </EthersAppContext>
+  <EthComponentsSettingsContext.Provider value={ethComponentsSettings}>
+    <ContractsAppContext>
+      <EthersAppContext>
+        <ChakraProvider theme={theme}>
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <Suspense fallback={<LoadingIndicator fullScreen />}>
+                <Routes>
+                  {Marketing}
+                  {Borrowers}
+                </Routes>
+              </Suspense>
+            </Router>
+          </QueryClientProvider>
+        </ChakraProvider>
+      </EthersAppContext>
+    </ContractsAppContext>
+  </EthComponentsSettingsContext.Provider>
 );
 
 export { App };
