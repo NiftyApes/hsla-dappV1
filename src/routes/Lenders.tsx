57dced7008df5aa0d0b7bd5eb7605cb3d22f58db
rkfsdev <prodev1104@gmail.com>
2022-04-20 08:46:57 -0500

    Add Lend page

--- src/routes/Lenders.tsx
+++ src/routes/Lenders.tsx
@@ -2,15 +2,17 @@ import { lazy } from 'react';
 import { Route } from 'react-router-dom';
 
 import LendersLayout from 'components/layout/Lenders';
-import { lenders, ROUTE_LENDERS_DASHBOARD } from './router';
+import { lenders, ROUTE_LENDERS_DASHBOARD, ROUTE_LENDERS_LEND } from './router';
 
 const LendersHome = lazy(() => import('../pages/lenders/Home'));
 const Dashboard = lazy(() => import('../pages/lenders/Dashboard'));
+const Lend = lazy(() => import('../pages/lenders/Lend'));
 
 const Lenders = (
   <Route path={lenders()} element={<LendersLayout />}>
     <Route path=":id" element={<LendersHome />} />
     <Route path={`:id/${ROUTE_LENDERS_DASHBOARD}`} element={<Dashboard />} />
+    <Route path={`:id/${ROUTE_LENDERS_LEND}`} element={<Lend />} />
   </Route>
 );
 
