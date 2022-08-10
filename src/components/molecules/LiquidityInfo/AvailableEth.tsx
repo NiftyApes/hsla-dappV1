4efd6e747edf123c3cbc22ba99c350f78e67dc00
Zachary Miller <zachary.john.miller@gmail.com>
2022-08-10 11:49:43 -0600

    Remove unused components

--- src/components/molecules/LiquidityInfo/AvailableEth.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@chakra-ui/react';
-import { useAvailableEthLiquidity } from 'hooks/useEthLiquidity';
-
-export const AvailableEth: React.FC = () => {
-  const { ethLiquidity } = useAvailableEthLiquidity();
-
-  return <Box>AvailableETH: {ethLiquidity}</Box>;
-};
