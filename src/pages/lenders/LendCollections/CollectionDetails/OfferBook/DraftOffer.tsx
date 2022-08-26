5acafdcde44206b37fb126d12c18263f21ac3ba4
Zachary Miller <zachary.john.miller@gmail.com>
2022-08-26 07:57:16 -0600

    Factor out DraftOffer into file, add prop types

---
+++ src/pages/lenders/LendCollections/CollectionDetails/OfferBook/DraftOffer.tsx
@@ -0,0 +1,91 @@
+import { Flex, Td, Text, Tr } from '@chakra-ui/react';
+import CryptoIcon from 'components/atoms/CryptoIcon';
+import moment from 'moment';
+
+interface DraftOfferProps {
+  collectionOfferAmt: number;
+  apr: number;
+  duration: number;
+  expirationInSeconds: number;
+}
+
+export const DraftOffer = ({
+  collectionOfferAmt,
+  apr,
+  duration,
+  expirationInSeconds,
+}: DraftOfferProps) => (
+  <>
+    <Tr
+      sx={{
+        backgroundColor: 'transparent !important',
+        height: 0,
+        td: { border: 'none' },
+      }}
+    >
+      <Td></Td>
+      <Td></Td>
+      <Td></Td>
+      <Td></Td>
+      <Td></Td>
+    </Tr>
+
+    <Tr
+      sx={{
+        /*
+Can't border-radius a table row :(
+Complicated workaround https://stackoverflow.com/questions/4094126/how-to-add-border-radius-on-table-row
+Pretty clearly not worth spending a day or so on this
+*/
+        backgroundColor: '#fff !important',
+        border: '2px solid #00a2ff',
+        mt: '24px',
+        td: {
+          border: 'none',
+          fontSize: 'md',
+          textAlign: 'center',
+        },
+        boxShadow: '0px 4px 24px 0px #4910921A',
+      }}
+    >
+      <Td sx={{ position: 'relative' }}>
+        <Flex alignItems="center" justifyContent="center">
+          <CryptoIcon symbol="eth" size={16} />
+          <Text ml="8px">{collectionOfferAmt}</Text>
+        </Flex>
+        <Flex
+          sx={{
+            alignItems: 'center',
+            background: '#00a2ff',
+            borderRadius: '4px 4px 0 0',
+            color: '#fff',
+            fontSize: '12px',
+            fontWeight: 600,
+            height: '24px',
+            justifyContent: 'center',
+            padding: '4px 8px',
+            position: 'absolute',
+            top: '-26px',
+            left: '24px',
+            width: '120px',
+          }}
+        >
+          DRAFT OFFER
+        </Flex>
+      </Td>
+      <Td>
+        <Text>{apr}%</Text>
+      </Td>
+      <Td>
+        <Text>
+          {duration && moment.duration(duration, 'days').asDays()} day
+          {moment.duration(duration, 'days').asDays() !== 1 && 's'}
+        </Text>
+      </Td>
+      <Td>
+        <Text>{moment(expirationInSeconds + Date.now()).format('h:mma, MMM D YYYY')}</Text>
+      </Td>
+      <Td></Td>
+    </Tr>
+  </>
+);
--- src/pages/lenders/LendCollections/CollectionDetails/OfferBook/index.tsx
+++ src/pages/lenders/LendCollections/CollectionDetails/OfferBook/index.tsx
@@ -11,82 +11,15 @@ import moment from 'moment';
 import React, { useState } from 'react';
 import { FaSort } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
+import { DraftOffer } from './DraftOffer';
 
-const DraftOffer = ({ collectionOfferAmt, apr, duration, expirationInSeconds, index }: any) => (
-  <>
-    <Tr
-      sx={{
-        backgroundColor: 'transparent !important',
-        height: 0,
-        td: { border: 'none' },
-      }}
-    >
-      <Td></Td>
-      <Td></Td>
-      <Td></Td>
-      <Td></Td>
-      <Td></Td>
-    </Tr>
-
-    <Tr
-      sx={{
-        /*
-Can't border-radius a table row :(
-Complicated workaround https://stackoverflow.com/questions/4094126/how-to-add-border-radius-on-table-row
-Pretty clearly not worth spending a day or so on this
-*/
-        backgroundColor: '#fff !important',
-        border: '2px solid #00a2ff',
-        mt: '24px',
-        td: {
-          border: 'none',
-          fontSize: 'md',
-          textAlign: 'center',
-        },
-        boxShadow: '0px 4px 24px 0px #4910921A',
-      }}
-    >
-      <Td sx={{ position: 'relative' }}>
-        <Flex alignItems="center" justifyContent="center">
-          <CryptoIcon symbol="eth" size={16} />
-          <Text ml="8px">{collectionOfferAmt}</Text>
-        </Flex>
-        <Flex
-          sx={{
-            alignItems: 'center',
-            background: '#00a2ff',
-            borderRadius: '4px 4px 0 0',
-            color: '#fff',
-            fontSize: '12px',
-            fontWeight: 600,
-            height: '24px',
-            justifyContent: 'center',
-            padding: '4px 8px',
-            position: 'absolute',
-            top: '-26px',
-            left: '24px',
-            width: '120px',
-          }}
-        >
-          DRAFT OFFER
-        </Flex>
-      </Td>
-      <Td>
-        <Text>{apr}%</Text>
-      </Td>
-      <Td>
-        <Text>
-          {duration && moment.duration(duration, 'days').asDays()} day
-          {moment.duration(duration, 'days').asDays() !== 1 && 's'}
-        </Text>
-      </Td>
-      <Td>
-        <Text>{moment(expirationInSeconds + Date.now()).format('h:mma, MMM D YYYY')}</Text>
-      </Td>
-      <Td></Td>
-    </Tr>
-  </>
-);
+interface OfferBook {
+  collectionOfferAmt: string;
+  apr: string;
+  duration: string;
+  expiration: string;
+  newlyAddedOfferHashes: string[];
+}
 
 const OfferBook: React.FC<any> = ({
   collectionOfferAmt,
@@ -335,7 +268,6 @@ const OfferBook: React.FC<any> = ({
             offer === 'DRAFT_OFFER' ? (
               <DraftOffer
                 key={i}
-                index={i}
                 collectionOfferAmt={collectionOfferAmt}
                 apr={apr}
                 duration={duration}
