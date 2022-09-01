d3539ad62bb35a0173c68baec54fef8a811713b7
Zachary Miller <zachary.john.miller@gmail.com>
2022-09-01 08:49:30 -0600

    Undoing lender dashboard changes that snuck in

--- src/pages/lenders/Dashboard/index.tsx
+++ src/pages/lenders/Dashboard/index.tsx
@@ -3,14 +3,11 @@ import React from 'react';
 
 import CryptoIcon from 'components/atoms/CryptoIcon';
 import TopCard from 'components/molecules/DashboardTopCard';
-import { useActiveLoansForLender } from 'hooks/useActiveLoansForLender';
-import LoansTable from './LoansTable';
+import LoanTable from './LoanTable';
 
 const Dashboard: React.FC = () => {
-  const activeLoans = useActiveLoansForLender();
   return (
     <Box>
-      {JSON.stringify(activeLoans, void 0, 2)}
       <Flex justifyContent="space-evenly" flexWrap="wrap" gap="24px" p="18px">
         <TopCard desc="ACTIVE LOAN">
           <Text fontSize="7xl">1</Text>
@@ -31,7 +28,7 @@ const Dashboard: React.FC = () => {
           <Text fontSize="5xl">⏰ 119 days, 23 hours, 59 minutes</Text>
         </TopCard>
       </Flex>
-      <LoansTable />
+      <LoanTable />
     </Box>
   );
 };
