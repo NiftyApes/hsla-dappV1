2965d81ac76f0b1e36dcd08249c61f412a28d92b
Zachary Miller <zachary.john.miller@gmail.com>
2022-09-05 09:27:33 -0600

    Add Nouns to transfer real NFTs functionality

--- src/helpers/getTokenIdRangeForLocalForksOfNftContracts.ts
+++ src/helpers/getTokenIdRangeForLocalForksOfNftContracts.ts
@@ -2,8 +2,9 @@ import {
   BAYC_CONTRACT_ADDRESS,
   DOODLES_CONTRACT_ADDRESS,
   MAYC_CONTRACT_ADDRESS,
+  NOUNS_CONTRACT_ADDRESS,
 } from 'constants/contractAddresses';
-
+import _ from 'lodash';
 // This function just limits what we iterate over
 // to a small range containing the specific NFTs we use locally
 
@@ -14,23 +15,15 @@ export const getTokenIdRangeForLocalForksOfNftContracts = ({
   nftContractAddress: string;
   totalSupply: number;
 }) => {
-  const startI =
-    nftContractAddress.toUpperCase() === BAYC_CONTRACT_ADDRESS.toUpperCase()
-      ? 860
-      : nftContractAddress.toUpperCase() === MAYC_CONTRACT_ADDRESS.toUpperCase()
-      ? 11863
-      : nftContractAddress.toUpperCase() === DOODLES_CONTRACT_ADDRESS.toUpperCase()
-      ? 5698
-      : 0;
-
-  const endI =
-    nftContractAddress.toUpperCase() === BAYC_CONTRACT_ADDRESS.toUpperCase()
-      ? 865
-      : nftContractAddress.toUpperCase() === MAYC_CONTRACT_ADDRESS.toUpperCase()
-      ? 11870
-      : nftContractAddress.toUpperCase() === DOODLES_CONTRACT_ADDRESS.toUpperCase()
-      ? 5703
-      : totalSupply;
-
-  return [startI, endI];
+  return nftContractAddress.toUpperCase() === BAYC_CONTRACT_ADDRESS.toUpperCase()
+    ? [861, 862, 863]
+    : nftContractAddress.toUpperCase() === MAYC_CONTRACT_ADDRESS.toUpperCase()
+    ? [11863, 11864, 11866]
+    : nftContractAddress.toUpperCase() === DOODLES_CONTRACT_ADDRESS.toUpperCase()
+    ? [5698, 5699, 5701]
+    : nftContractAddress.toUpperCase() === NOUNS_CONTRACT_ADDRESS.toUpperCase()
+    ? [190, 213, 260]
+    : totalSupply > 0
+    ? _.range(0, totalSupply - 1)
+    : [];
 };
