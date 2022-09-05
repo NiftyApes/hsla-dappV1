77a6e6bd0b3aa92c9774de37f9ccd58b36e5f6d5
Zachary Miller <zachary.john.miller@gmail.com>
2022-09-05 09:35:22 -0600

    Center transaction history table

--- src/components/molecules/LenderTransactionHistory/index.tsx
+++ src/components/molecules/LenderTransactionHistory/index.tsx
@@ -1,10 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Center } from '@chakra-ui/react';
 import { TransactionTable } from './TransactionTable';
 
 export const LenderTransactionHistory: React.FC = () => {
   return (
-    <Box maxW="1600px" minW="700px">
-      <TransactionTable />
-    </Box>
+    <Center>
+      <Box maxW="1600px" minW="700px">
+        <TransactionTable />
+      </Box>
+    </Center>
   );
 };
