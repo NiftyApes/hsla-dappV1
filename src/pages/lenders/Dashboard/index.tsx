8e705b5a60581bf817b896cd06eca273fc9ba9d4
Zachary Miller <zachary.john.miller@gmail.com>
2022-09-12 16:00:41 -0600

    Lender dashboard top bar

--- src/pages/lenders/Dashboard/index.tsx
+++ src/pages/lenders/Dashboard/index.tsx
@@ -1,13 +1,12 @@
-import { Box, Center, Flex, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Box, Center, Grid, GridItem, Text } from '@chakra-ui/react';
 import React from 'react';
 
-import CryptoIcon from 'components/atoms/CryptoIcon';
-import TopCard from 'components/molecules/DashboardTopCard';
 import { useActiveLoansForLender } from 'hooks/useActiveLoansForLender';
 import { useOffersForLender } from 'hooks/useOffersOfLender';
 import { useWalletAddress } from 'hooks/useWalletAddress';
 import LoansTable from './LoansTable';
 import OffersTable from './OffersTable';
+import TopBar from './TopBar';
 
 const Dashboard: React.FC = () => {
   const address = useWalletAddress();
@@ -18,26 +17,7 @@ const Dashboard: React.FC = () => {
 
   return (
     <Box>
-      <Flex justifyContent="space-evenly" flexWrap="wrap" gap="24px" p="18px">
-        <TopCard desc="ACTIVE LOAN">
-          <Text fontSize="7xl">1</Text>
-        </TopCard>
-        <TopCard desc="TOTAL BORROWED">
-          <CryptoIcon symbol="eth" size={40} />
-          <Text fontSize="7xl" ml="8px">
-            25.50Ξ
-          </Text>
-        </TopCard>
-        <TopCard desc="TOTAL INTEREST OWED">
-          <CryptoIcon symbol="eth" size={40} />
-          <Text fontSize="7xl" ml="8px">
-            0.026..Ξ
-          </Text>
-        </TopCard>
-        <TopCard desc="NEXT PAYMENT DUE">
-          <Text fontSize="5xl">⏰ 119 days, 23 hours, 59 minutes</Text>
-        </TopCard>
-      </Flex>
+      <TopBar />
       <Center px="36px">
         <Grid templateColumns="800px 700px" columnGap="24px">
           <GridItem>
