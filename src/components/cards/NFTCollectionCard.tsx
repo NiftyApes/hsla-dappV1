adb95b99ba0eb6be1736c9b242f58421f875d521
andreitr <andrei.taraschuk@gmail.com>
2022-10-02 12:10:26 -0600

    Default collections

--- src/components/cards/NFTCollectionCard.tsx
+++ src/components/cards/NFTCollectionCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoadingIndicator from "../atoms/LoadingIndicator";
-import {Box, Flex, Image, Text} from "@chakra-ui/react";
+import {Flex, Image, Text} from "@chakra-ui/react";
 import {useCollectionMetadata} from "../../hooks/useCollectionMetadata";
 import {useCollectionStats} from "../../hooks/useColectionStats";
 
@@ -11,24 +11,27 @@ interface Props {
 
 const NFTCollectionCard: React.FC<Props> = ({contractAddress}) => {
 
+    // This is option.
     const fetchedNFT: any = useCollectionMetadata({
         nftContractAddress: contractAddress,
     });
 
-    const {floorPrice} = useCollectionStats({nftContractAddress: contractAddress});
+    const {floorPrice, volume} = useCollectionStats({nftContractAddress: contractAddress});
 
     if (!fetchedNFT.image) {
         return <LoadingIndicator size={'md'}/>;
     }
 
     return (
-
         <Flex flexDir="row" p="10px">
             <Image src={fetchedNFT.image} w="55px" h="55px" borderRadius="full"/>
             <Flex flexDir="column" ml="10px">
                 <Text fontWeight='bold'>{fetchedNFT.name}</Text>
                 <Text>{fetchedNFT.address}</Text>
-                <Text>Floor price: {floorPrice}</Text>
+                <Flex flexDir="column">
+                    <Text>Floor price: {floorPrice}</Text>
+                    <Text>Volume: {volume}</Text>
+                </Flex>
             </Flex>
         </Flex>
     );
--- src/pages/lenders/LendCollections/index.tsx
+++ src/pages/lenders/LendCollections/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import {
-    Flex,
-    Image,
-    Text,
     ModalContent,
     ModalOverlay,
     Modal,
