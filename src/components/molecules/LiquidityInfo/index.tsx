3359e38dea09776b59653476a5cc90585327cdaa
andreitr <andrei.taraschuk@gmail.com>
2022-10-05 15:01:19 -0600

    Disconnected wallet message on liquidity page

--- src/components/molecules/LiquidityInfo/index.tsx
+++ src/components/molecules/LiquidityInfo/index.tsx
@@ -1,28 +1,44 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { useAvailableEthLiquidity } from 'hooks/useEthLiquidity';
-import { useTotalEthLoanedOut } from 'hooks/useTotalEthLoanedOut';
+import {Box, Flex, Text} from '@chakra-ui/react';
+import {useAvailableEthLiquidity} from 'hooks/useEthLiquidity';
+import {useTotalEthLoanedOut} from 'hooks/useTotalEthLoanedOut';
 import _ from 'lodash';
-import { EthLiquidityInfo } from './EthLiquidityInfo';
-import { LiquidityInfoLoading } from './LiquidityInfoLoading';
-import { LiquidityPieChart } from './LiquidityPieChart';
+import {EthLiquidityInfo} from './EthLiquidityInfo';
+import {LiquidityInfoLoading} from './LiquidityInfoLoading';
+import {LiquidityPieChart} from './LiquidityPieChart';
+import {useWalletAddress} from "../../../hooks/useWalletAddress";
+import {EmptyPlaceholder} from "../../cards/EmptyPlaceholder";
+
+const i18n = {
+disconnected: 'Please connect your wallet to proceed...'
+}
 
 export const LiquidityInfo: React.FC = () => {
-  const { totalEthLoanedOut } = useTotalEthLoanedOut();
+    const address = useWalletAddress();
+    const {totalEthLoanedOut} = useTotalEthLoanedOut();
+    const {availableEthLiquidity} = useAvailableEthLiquidity();
 
-  const { availableEthLiquidity } = useAvailableEthLiquidity();
+    // Disconnected view
+    if (_.isUndefined(address)) {
+        return (
+            <EmptyPlaceholder>
+                <Text>{i18n.disconnected}</Text>
+            </EmptyPlaceholder>
+        );
+    }
 
-  return (
-    <Box>
-      {_.isNil(availableEthLiquidity) || _.isNil(totalEthLoanedOut) ? (
-        <LiquidityInfoLoading />
-      ) : (
-        <Flex>
-          <LiquidityPieChart />
-          <Box ml="40px" mt="40px">
-            <EthLiquidityInfo />
-          </Box>
-        </Flex>
-      )}
-    </Box>
-  );
+
+    return (
+        <Box>
+            {_.isNil(availableEthLiquidity) || _.isNil(totalEthLoanedOut) ? (
+                <LiquidityInfoLoading/>
+            ) : (
+                <Flex>
+                    <LiquidityPieChart/>
+                    <Box ml="40px" mt="40px">
+                        <EthLiquidityInfo/>
+                    </Box>
+                </Flex>
+            )}
+        </Box>
+    );
 };
--- src/generated/contract-types/ILendingAdmin.ts
+++ src/generated/contract-types/ILendingAdmin.ts
@@ -12,68 +12,92 @@ import {
   PopulatedTransaction,
   Signer,
   utils,
-} from 'ethers';
-import { FunctionFragment, Result } from '@ethersproject/abi';
-import { Listener, Provider } from '@ethersproject/providers';
-import { TypedEventFilter, TypedEvent, TypedListener, OnEvent } from './common';
+} from "ethers";
+import { FunctionFragment, Result } from "@ethersproject/abi";
+import { Listener, Provider } from "@ethersproject/providers";
+import { TypedEventFilter, TypedEvent, TypedListener, OnEvent } from "./common";
 
 export interface ILendingAdminInterface extends utils.Interface {
-  contractName: 'ILendingAdmin';
+  contractName: "ILendingAdmin";
   functions: {
-    'pause()': FunctionFragment;
-    'pauseSanctions()': FunctionFragment;
-    'unpause()': FunctionFragment;
-    'unpauseSanctions()': FunctionFragment;
-    'updateDefaultRefinancePremiumBps(uint16)': FunctionFragment;
-    'updateGasGriefingPremiumBps(uint16)': FunctionFragment;
-    'updateOriginationPremiumLenderBps(uint16)': FunctionFragment;
-    'updateProtocolInterestBps(uint16)': FunctionFragment;
-    'updateTermGriefingPremiumBps(uint16)': FunctionFragment;
+    "pause()": FunctionFragment;
+    "pauseSanctions()": FunctionFragment;
+    "unpause()": FunctionFragment;
+    "unpauseSanctions()": FunctionFragment;
+    "updateDefaultRefinancePremiumBps(uint16)": FunctionFragment;
+    "updateGasGriefingPremiumBps(uint16)": FunctionFragment;
+    "updateOriginationPremiumLenderBps(uint16)": FunctionFragment;
+    "updateProtocolInterestBps(uint16)": FunctionFragment;
+    "updateTermGriefingPremiumBps(uint16)": FunctionFragment;
   };
 
-  encodeFunctionData(functionFragment: 'pause', values?: undefined): string;
-  encodeFunctionData(functionFragment: 'pauseSanctions', values?: undefined): string;
-  encodeFunctionData(functionFragment: 'unpause', values?: undefined): string;
-  encodeFunctionData(functionFragment: 'unpauseSanctions', values?: undefined): string;
+  encodeFunctionData(functionFragment: "pause", values?: undefined): string;
   encodeFunctionData(
-    functionFragment: 'updateDefaultRefinancePremiumBps',
-    values: [BigNumberish],
+    functionFragment: "pauseSanctions",
+    values?: undefined
+  ): string;
+  encodeFunctionData(functionFragment: "unpause", values?: undefined): string;
+  encodeFunctionData(
+    functionFragment: "unpauseSanctions",
+    values?: undefined
   ): string;
   encodeFunctionData(
-    functionFragment: 'updateGasGriefingPremiumBps',
-    values: [BigNumberish],
+    functionFragment: "updateDefaultRefinancePremiumBps",
+    values: [BigNumberish]
   ): string;
   encodeFunctionData(
-    functionFragment: 'updateOriginationPremiumLenderBps',
-    values: [BigNumberish],
+    functionFragment: "updateGasGriefingPremiumBps",
+    values: [BigNumberish]
   ): string;
-  encodeFunctionData(functionFragment: 'updateProtocolInterestBps', values: [BigNumberish]): string;
   encodeFunctionData(
-    functionFragment: 'updateTermGriefingPremiumBps',
-    values: [BigNumberish],
+    functionFragment: "updateOriginationPremiumLenderBps",
+    values: [BigNumberish]
+  ): string;
+  encodeFunctionData(
+    functionFragment: "updateProtocolInterestBps",
+    values: [BigNumberish]
+  ): string;
+  encodeFunctionData(
+    functionFragment: "updateTermGriefingPremiumBps",
+    values: [BigNumberish]
   ): string;
 
-  decodeFunctionResult(functionFragment: 'pause', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'pauseSanctions', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'unpause', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'unpauseSanctions', data: BytesLike): Result;
+  decodeFunctionResult(functionFragment: "pause", data: BytesLike): Result;
   decodeFunctionResult(
-    functionFragment: 'updateDefaultRefinancePremiumBps',
-    data: BytesLike,
+    functionFragment: "pauseSanctions",
+    data: BytesLike
   ): Result;
-  decodeFunctionResult(functionFragment: 'updateGasGriefingPremiumBps', data: BytesLike): Result;
+  decodeFunctionResult(functionFragment: "unpause", data: BytesLike): Result;
   decodeFunctionResult(
-    functionFragment: 'updateOriginationPremiumLenderBps',
-    data: BytesLike,
+    functionFragment: "unpauseSanctions",
+    data: BytesLike
   ): Result;
-  decodeFunctionResult(functionFragment: 'updateProtocolInterestBps', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'updateTermGriefingPremiumBps', data: BytesLike): Result;
+  decodeFunctionResult(
+    functionFragment: "updateDefaultRefinancePremiumBps",
+    data: BytesLike
+  ): Result;
+  decodeFunctionResult(
+    functionFragment: "updateGasGriefingPremiumBps",
+    data: BytesLike
+  ): Result;
+  decodeFunctionResult(
+    functionFragment: "updateOriginationPremiumLenderBps",
+    data: BytesLike
+  ): Result;
+  decodeFunctionResult(
+    functionFragment: "updateProtocolInterestBps",
+    data: BytesLike
+  ): Result;
+  decodeFunctionResult(
+    functionFragment: "updateTermGriefingPremiumBps",
+    data: BytesLike
+  ): Result;
 
   events: {};
 }
 
 export interface ILendingAdmin extends BaseContract {
-  contractName: 'ILendingAdmin';
+  contractName: "ILendingAdmin";
   connect(signerOrProvider: Signer | Provider | string): this;
   attach(addressOrName: string): this;
   deployed(): Promise<this>;
@@ -83,14 +107,16 @@ export interface ILendingAdmin extends BaseContract {
   queryFilter<TEvent extends TypedEvent>(
     event: TypedEventFilter<TEvent>,
     fromBlockOrBlockhash?: string | number | undefined,
-    toBlock?: string | number | undefined,
+    toBlock?: string | number | undefined
   ): Promise<Array<TEvent>>;
 
   listeners<TEvent extends TypedEvent>(
-    eventFilter?: TypedEventFilter<TEvent>,
+    eventFilter?: TypedEventFilter<TEvent>
   ): Array<TypedListener<TEvent>>;
   listeners(eventName?: string): Array<Listener>;
-  removeAllListeners<TEvent extends TypedEvent>(eventFilter: TypedEventFilter<TEvent>): this;
+  removeAllListeners<TEvent extends TypedEvent>(
+    eventFilter: TypedEventFilter<TEvent>
+  ): this;
   removeAllListeners(eventName?: string): this;
   off: OnEvent<this>;
   on: OnEvent<this>;
@@ -99,84 +125,86 @@ export interface ILendingAdmin extends BaseContract {
 
   functions: {
     pause(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     pauseSanctions(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     unpause(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     unpauseSanctions(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     updateDefaultRefinancePremiumBps(
       newDefaultRefinancePremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     updateGasGriefingPremiumBps(
       newGasGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     updateOriginationPremiumLenderBps(
       newOriginationPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     updateProtocolInterestBps(
       newProtocolInterestBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
 
     updateTermGriefingPremiumBps(
       newTermGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<ContractTransaction>;
   };
 
-  pause(overrides?: Overrides & { from?: string | Promise<string> }): Promise<ContractTransaction>;
+  pause(
+    overrides?: Overrides & { from?: string | Promise<string> }
+  ): Promise<ContractTransaction>;
 
   pauseSanctions(
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   unpause(
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   unpauseSanctions(
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   updateDefaultRefinancePremiumBps(
     newDefaultRefinancePremiumBps: BigNumberish,
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   updateGasGriefingPremiumBps(
     newGasGriefingPremiumBps: BigNumberish,
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   updateOriginationPremiumLenderBps(
     newOriginationPremiumBps: BigNumberish,
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   updateProtocolInterestBps(
     newProtocolInterestBps: BigNumberish,
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   updateTermGriefingPremiumBps(
     newTermGriefingPremiumBps: BigNumberish,
-    overrides?: Overrides & { from?: string | Promise<string> },
+    overrides?: Overrides & { from?: string | Promise<string> }
   ): Promise<ContractTransaction>;
 
   callStatic: {
@@ -190,109 +218,115 @@ export interface ILendingAdmin extends BaseContract {
 
     updateDefaultRefinancePremiumBps(
       newDefaultRefinancePremiumBps: BigNumberish,
-      overrides?: CallOverrides,
+      overrides?: CallOverrides
     ): Promise<void>;
 
     updateGasGriefingPremiumBps(
       newGasGriefingPremiumBps: BigNumberish,
-      overrides?: CallOverrides,
+      overrides?: CallOverrides
     ): Promise<void>;
 
     updateOriginationPremiumLenderBps(
       newOriginationPremiumBps: BigNumberish,
-      overrides?: CallOverrides,
+      overrides?: CallOverrides
     ): Promise<void>;
 
     updateProtocolInterestBps(
       newProtocolInterestBps: BigNumberish,
-      overrides?: CallOverrides,
+      overrides?: CallOverrides
     ): Promise<void>;
 
     updateTermGriefingPremiumBps(
       newTermGriefingPremiumBps: BigNumberish,
-      overrides?: CallOverrides,
+      overrides?: CallOverrides
     ): Promise<void>;
   };
 
   filters: {};
 
   estimateGas: {
-    pause(overrides?: Overrides & { from?: string | Promise<string> }): Promise<BigNumber>;
+    pause(
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<BigNumber>;
 
-    pauseSanctions(overrides?: Overrides & { from?: string | Promise<string> }): Promise<BigNumber>;
+    pauseSanctions(
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<BigNumber>;
 
-    unpause(overrides?: Overrides & { from?: string | Promise<string> }): Promise<BigNumber>;
+    unpause(
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<BigNumber>;
 
     unpauseSanctions(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
 
     updateDefaultRefinancePremiumBps(
       newDefaultRefinancePremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
 
     updateGasGriefingPremiumBps(
       newGasGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
 
     updateOriginationPremiumLenderBps(
       newOriginationPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
 
     updateProtocolInterestBps(
       newProtocolInterestBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
 
     updateTermGriefingPremiumBps(
       newTermGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<BigNumber>;
   };
 
   populateTransaction: {
     pause(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     pauseSanctions(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     unpause(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     unpauseSanctions(
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     updateDefaultRefinancePremiumBps(
       newDefaultRefinancePremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     updateGasGriefingPremiumBps(
       newGasGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     updateOriginationPremiumLenderBps(
       newOriginationPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     updateProtocolInterestBps(
       newProtocolInterestBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
 
     updateTermGriefingPremiumBps(
       newTermGriefingPremiumBps: BigNumberish,
-      overrides?: Overrides & { from?: string | Promise<string> },
+      overrides?: Overrides & { from?: string | Promise<string> }
     ): Promise<PopulatedTransaction>;
   };
 }
