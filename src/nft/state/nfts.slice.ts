4920c1b77884dd8bcc59929bad77460db9fd52ad
Zachary Miller <zachary.john.miller@gmail.com>
2022-10-11 13:58:00 -0600

    Add NFTs to Redux state instead of replace

--- src/nft/state/nfts.slice.ts
+++ src/nft/state/nfts.slice.ts
@@ -152,7 +152,9 @@ const slice = createSlice({
         }
 
         if (action.payload.content) {
-          state.nftsByWalletAddress[action.meta.arg.walletAddress].content = action.payload.content;
+          state.nftsByWalletAddress[action.meta.arg.walletAddress].content?.push(
+            ...action.payload.content,
+          );
         }
 
         state.nftsByWalletAddress[action.meta.arg.walletAddress].fetching = false;
