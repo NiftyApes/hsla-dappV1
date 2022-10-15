b02a8d95b2bf9d8b288af5342d783217de39cd27
Zachary Miller <zachary.john.miller@gmail.com>
2022-10-14 20:33:56 -0600

    Provider for contracts when user not connected

--- src/helpers/getEthersContractWithEIP1193Provider.ts
+++ src/helpers/getEthersContractWithEIP1193Provider.ts
@@ -1,3 +1,4 @@
+import { AlchemyProvider } from '@ethersproject/providers';
 import { EIP1193Provider } from '@web3-onboard/core';
 import { ContractInterface, ethers } from 'ethers';
 
@@ -11,7 +12,22 @@ export function getEthersContractWithEIP1193Provider({
   provider: EIP1193Provider;
 }) {
   const ethersProvider = new ethers.providers.Web3Provider(provider);
-  const contract = new ethers.Contract(address, abi, ethersProvider.getUncheckedSigner());
 
-  return contract;
+  // Apparently passing an Alchemy URL to make an EIP-1193 provider
+  // doesn't work :( This is a super hacky way of allowing us to
+  // query contracts when the user isn't connected.
+  const isAlchemy = (ethersProvider.provider as any).connection.url.includes('alchemy');
+
+  if (isAlchemy) {
+    return new ethers.Contract(
+      address,
+      abi,
+      new AlchemyProvider(
+        (ethersProvider.provider as any).connection.url.split('.')[0].split('-')[1],
+        (ethersProvider.provider as any).connection.url.slice(-32),
+      ),
+    );
+  }
+
+  return new ethers.Contract(address, abi, ethersProvider);
 }
