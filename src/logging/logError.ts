3415bdfcb19d8813911afaecfe0296587d5ea65d
Zachary Miller <zachary.john.miller@gmail.com>
2022-10-25 14:10:33 -0600

    Refine Sentry & work on source maps setup

--- src/logging/logError.ts
+++ src/logging/logError.ts
@@ -2,6 +2,9 @@ import * as Sentry from '@sentry/react';
 
 export function logError(err: any) {
   console.error(err, JSON.stringify(err));
-  Sentry.captureException(err);
-  Sentry.captureMessage(JSON.stringify(err));
+  if (err instanceof Error) {
+    Sentry.captureException(err);
+  } else {
+    Sentry.captureMessage(JSON.stringify(err));
+  }
 }
