c4508cd6ff0f1da0abb783c14af0e597f2d5bd20
Antonio <hedhogs@gmail.com>
2022-10-27 17:01:52 +0200

    feat(borrowers): improve empty categories, improve unconnected wallet view

--- src/pages/borrowers/Home/Content/WalletNFTs.tsx
+++ src/pages/borrowers/Home/Content/WalletNFTs.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, SimpleGrid } from '@chakra-ui/react';
+import { Box, Center, Link, SimpleGrid, Text } from '@chakra-ui/react';
 import { useAppSelector } from 'app/hooks';
 import { RootState } from 'app/store';
 import SectionHeader from 'components/molecules/SectionHeader';
@@ -6,11 +6,13 @@ import { useWalletAddress } from 'hooks/useWalletAddress';
 import _ from 'lodash';
 import { NFT } from 'nft';
 import { useNFTsByWalletAddress } from 'nft/state/nfts.slice';
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
+import { WalletContext } from 'lib/contexts/WalletProvider';
 import LoadingIndicator from '../../../../components/atoms/LoadingIndicator';
 import { NFTCardContainer } from './NFTCardContainer';
 
 export const WalletNFTs: React.FC = () => {
+  const { connectWallet } = useContext(WalletContext);
   const walletAddress = useWalletAddress();
   const nfts = useNFTsByWalletAddress(walletAddress || '');
   const offers = useAppSelector(
@@ -88,48 +90,85 @@ export const WalletNFTs: React.FC = () => {
         </>
       )}
 
-      {_.isEmpty(NFTsWithOffers) ? null : (
-        <>
-          <Box my="16px">
-            <SectionHeader headerText="NFTs With Offers" />
-          </Box>
-          <SimpleGrid
-            columns={{ xl: 5, lg: 4, md: 3, sm: 2, xs: 1 }}
-            spacing={10}
-            style={{ padding: '16px' }}
-          >
-            {NFTsWithOffers?.map((item: any) => {
-              return (
-                <NFTCardContainer
-                  item={item}
-                  key={`${item.contractAddress}___${item.id}`}
-                />
-              );
-            })}
-          </SimpleGrid>
-        </>
+      <Box my="16px">
+        <SectionHeader headerText="NFTs With Offers" />
+      </Box>
+      {_.isEmpty(NFTsWithOffers) ? (
+        <Box my={10}>
+          {walletAddress ? (
+            <>
+              <Center>
+                <Text fontStyle="italic" color="gray.500">
+                  😢 No Offers Found For Your NFT 😢
+                </Text>
+              </Center>
+              <Center mt={4}>
+                <Link
+                  target="_blank"
+                  href="https://docs.niftyapes.money/borrowers-dapp-guide/borrow-page"
+                  fontWeight="bold"
+                  color="purple"
+                >
+                  How Does Borrowing Work?
+                </Link>
+              </Center>
+            </>
+          ) : (
+            <>
+              <Center>
+                <Text fontStyle="italic" color="gray.500">
+                  Connect Your Wallet to See All Liquidity Offers
+                </Text>
+              </Center>
+              <Center mt={4}>
+                <Link onClick={connectWallet} fontWeight="bold" color="purple">
+                  CONNECT YOUR WALLET
+                </Link>
+              </Center>
+            </>
+          )}
+        </Box>
+      ) : (
+        <SimpleGrid
+          columns={{ xl: 5, lg: 4, md: 3, sm: 2, xs: 1 }}
+          spacing={10}
+          style={{ padding: '16px' }}
+        >
+          {NFTsWithOffers?.map((item: any) => {
+            return (
+              <NFTCardContainer
+                item={item}
+                key={`${item.contractAddress}___${item.id}`}
+              />
+            );
+          })}
+        </SimpleGrid>
       )}
 
-      {_.isEmpty(NFTsWithNoOffers) ? null : (
-        <>
-          <Box my="16px">
-            <SectionHeader headerText="NFTs With No Offers" />
-          </Box>
-          <SimpleGrid
-            columns={{ xl: 5, lg: 4, md: 3, sm: 2, xs: 1 }}
-            spacing={10}
-            style={{ padding: '16px' }}
-          >
-            {NFTsWithNoOffers?.map((item: any) => {
-              return (
-                <NFTCardContainer
-                  item={item}
-                  key={`${item.contractAddress}___${item.id}`}
-                />
-              );
-            })}
-          </SimpleGrid>
-        </>
+      <Box my="16px">
+        <SectionHeader headerText="NFTs With No Offers" />
+      </Box>
+      {_.isEmpty(NFTsWithOffers) ? (
+        <Center my={10}>
+          <Text fontStyle="italic" color="gray.500">
+            No NFTs With No Offers
+          </Text>
+        </Center>
+      ) : (
+        <SimpleGrid
+          columns={{ xl: 5, lg: 4, md: 3, sm: 2, xs: 1 }}
+          spacing={10}
+          style={{ padding: '16px' }}
+        >
+          {NFTsWithNoOffers?.map((item: any) => {
+            return (
+              <NFTCardContainer
+                item={item}
+                key={`${item.contractAddress}___${item.id}`}
+              />
+            );
+          })}
+        </SimpleGrid>
       )}
     </>
   );
--- src/pages/borrowers/Home/index.tsx
+++ src/pages/borrowers/Home/index.tsx
@@ -1,17 +1,9 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 
-import { useWalletAddress } from 'hooks/useWalletAddress';
-import DisconnectedView from '../DisconnectedView';
 import Content from './Content';
 
 const Borrowers: React.FC = () => {
-  const walletAddress = useWalletAddress();
-
-  if (!walletAddress) {
-    return <DisconnectedView />;
-  }
-
   return (
     <Flex position="relative" flexGrow={1}>
       <Content width="100%" />
