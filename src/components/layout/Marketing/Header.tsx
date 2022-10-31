5882686dbafbe8275dc9c3cb59dce987885afa20
andreitr <andrei.taraschuk@gmail.com>
2022-10-31 17:28:10 -0600

    Landing page layout

--- src/components/layout/Marketing/Header.tsx
+++ src/components/layout/Marketing/Header.tsx
@@ -1,7 +1,131 @@
-import React from 'react';
+import {
+  Button,
+  Flex,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+import React, { useCallback } from 'react';
+import { AiOutlineCaretDown } from 'react-icons/ai';
+import { Link, useNavigate } from 'react-router-dom';
+
+import WalletInfo from 'components/molecules/WalletInfo';
+import {
+  borrowers,
+  borrowersDashboard,
+  lenders,
+  lendersDashboard,
+  lendersLiquidity,
+} from 'routes/router';
 
 const Header: React.FC = () => {
-  return <div>Marketing Header</div>;
+  const navigate = useNavigate();
+
+  const navigateToLendersIdDashboard = useCallback(() => {
+    navigate(lendersDashboard());
+  }, [navigate]);
+
+  const navigateToLendersIdLiquidity = useCallback(() => {
+    navigate(lendersLiquidity());
+  }, [navigate]);
+
+  const navigateToLendersIdLend = useCallback(() => {
+    navigate(lenders());
+  }, [navigate]);
+
+  const navigateToBorrowersDashboard = useCallback(() => {
+    navigate(borrowersDashboard());
+  }, [navigate]);
+
+  const navigateToBorrowers = useCallback(() => {
+    navigate(borrowers());
+  }, [navigate]);
+
+  return (
+    <Flex
+      p="20px 15px 32px 20px"
+      alignItems="center"
+      justifyContent="space-between"
+      top="0px"
+      zIndex={9}
+    >
+      <Flex alignItems="center">
+        <Link to="/">
+          <Image
+            src="/assets/images/header_logo.png"
+            alt="Logo"
+            mr="15px"
+            cursor="pointer"
+          />
+        </Link>
+        <Menu key={window.location.pathname}>
+          <MenuButton
+            as={Button}
+            rightIcon={<AiOutlineCaretDown />}
+            bg="transparent"
+            noOfLines={1}
+            fontSize={{ base: '14px', xl: '16px' }}
+          >
+            BORROWERS
+          </MenuButton>
+          <MenuList
+            borderRadius="15px"
+            boxShadow="0px 0px 21px rgba(58, 0, 131, 0.1)"
+            p="9px 7px"
+            fontSize="md"
+            sx={{
+              button: {
+                fontWeight: 'bold',
+                borderRadius: '10px',
+                p: '15px',
+              },
+            }}
+          >
+            <MenuItem onClick={navigateToBorrowersDashboard}>
+              📊 Dashboard
+            </MenuItem>
+            <MenuItem onClick={navigateToBorrowers}>🍌 Borrow</MenuItem>
+          </MenuList>
+        </Menu>
+        <Menu>
+          <MenuButton
+            as={Button}
+            rightIcon={<AiOutlineCaretDown />}
+            bg="transparent"
+            noOfLines={1}
+            fontSize={{ base: '14px', xl: '16px' }}
+          >
+            LENDERS
+          </MenuButton>
+          <MenuList
+            borderRadius="15px"
+            boxShadow="0px 0px 21px rgba(58, 0, 131, 0.1)"
+            p="9px 7px"
+            sx={{
+              button: {
+                fontWeight: 'bold',
+                borderRadius: '10px',
+                p: '15px',
+              },
+            }}
+          >
+            <MenuItem onClick={navigateToLendersIdDashboard}>
+              📊 Dashboard
+            </MenuItem>
+            <MenuItem onClick={navigateToLendersIdLiquidity}>
+              💧 Manage Liquidity
+            </MenuItem>
+            <MenuItem onClick={navigateToLendersIdLend}>
+              📜 Create Offers
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+      <WalletInfo />
+    </Flex>
+  );
 };
 
 export default Header;
