c9fbc8263861997b60f0b5dad294a101e9ae063e
Zachary Miller <zachary.john.miller@gmail.com>
2022-10-31 15:57:02 -0600

    Remove WalletConnect for now

--- src/services/wallet.ts
+++ src/services/wallet.ts
@@ -1,13 +1,10 @@
 import injectedModule from '@web3-onboard/injected-wallets';
 import { init } from '@web3-onboard/react';
-import walletConnectModule from '@web3-onboard/walletconnect';
 
 const injected = injectedModule();
 
-const walletConnect = walletConnectModule();
-
 export const initWeb3Onboard = init({
-  wallets: [injected, walletConnect],
+  wallets: [injected],
   chains: [
     {
       id: '0x1',
