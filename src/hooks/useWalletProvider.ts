6a986f95c8eeddcac96d9b16c04020ea1daef205
Zachary Miller <zachary.john.miller@gmail.com>
2022-10-31 23:53:52 -0600

    One-off fix for offers contract when unconnected

--- src/hooks/useWalletProvider.ts
+++ src/hooks/useWalletProvider.ts
@@ -1,23 +1,11 @@
 import { useConnectWallet } from '@web3-onboard/react';
-import Provider from 'eip1193-provider';
-import { useChainId } from './useChainId';
 
-export const useWalletProvider = () => {
+export function useWalletProvider() {
   const [{ wallet }] = useConnectWallet();
-
-  const chainId = useChainId();
 
   if (wallet?.provider) {
     return wallet?.provider;
   }
 
-  return chainId === '0x7a69'
-    ? new Provider('http://localhost:8545')
-    : chainId === '0x5'
-    ? new Provider(
-        'https://eth-goerli.g.alchemy.com/v2/_8-YJwskeUfHw9RMaQufSFupoqkd_ukD',
-      )
-    : new Provider(
-        'https://eth-mainnet.g.alchemy.com/v2/jxUUn2DsYODlc68SEU_7eNGCn2hQ7b63',
-      );
-};
+  return undefined;
+}
