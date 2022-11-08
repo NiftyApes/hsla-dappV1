d163c154854e5068b283c36d404b01058f4b3ab4
Antonio <hedhogs@gmail.com>
2022-11-08 17:01:03 +0100

    feat(ga): add ga4, remove old ga

--- src/hooks/useAnalyticsEventTracker.ts
+++ src/hooks/useAnalyticsEventTracker.ts
@@ -1,4 +1,4 @@
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 export const useAnalyticsEventTracker = (category: string) => {
   const eventTracker = (action: string, label: string) => {
