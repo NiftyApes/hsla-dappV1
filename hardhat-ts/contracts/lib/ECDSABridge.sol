e34a4ddd18798e0f4689bfe003f5a6af4a28e38d
Zachary Miller <zachary.john.miller@gmail.com>
2022-11-21 08:44:11 -0700

    Return ECDSABridge to previous

--- hardhat-ts/contracts/lib/ECDSABridge.sol
+++ hardhat-ts/contracts/lib/ECDSABridge.sol
@@ -3,14 +3,10 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 
-import "hardhat/console.sol";
-
 /// @title Moves OpenZeppelin's ECDSA implementation into a separate library to save
 ///        code size in the main contract.
 library ECDSABridge {
-  using ECDSAUpgradeable for bytes32;
-
-  function recover(bytes32 hash, bytes memory signature) internal view returns (address) {
-    return hash.recover(signature);
+  function recover(bytes32 hash, bytes memory signature) internal pure returns (address) {
+    return ECDSAUpgradeable.recover(hash, signature);
   }
 }
