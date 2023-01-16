aac3f9125be17436c1f5496f8ccc9920f4dfe251
Antonio <hedhogs@gmail.com>
2023-01-16 17:02:46 +0100

    feat(rollover): add no payment needed status, add action label to offers list; #490

--- src/components/molecules/BorrowLoanRolloverCard/index.tsx
+++ src/components/molecules/BorrowLoanRolloverCard/index.tsx
@@ -83,6 +83,8 @@ const BorrowLoanRolloverCard: React.FC<Props> = ({
   const bestOffer: LoanOffer = getBestLoanOffer(offers);
   const [rolloverOffer, setRolloverOffer] = useState<LoanOffer>(bestOffer);
 
+  const [paymentSuccess] = useState(false);
+
   const {
     isOpen: isAllOffersOpen,
     onOpen: onAllOffersOpen,
@@ -377,8 +379,11 @@ const BorrowLoanRolloverCard: React.FC<Props> = ({
                     variant="outline"
                     padding="6"
                     _hover={{ backgroundColor: 'initial', cursor: 'default' }}
+                    _active={{ backgroundColor: 'initial', cursor: 'default' }}
                   >
-                    🥳 Payment Made 🎉
+                    {paymentSuccess
+                      ? '🥳 Payment Made 🎉'
+                      : 'No Payment Needed'}
                   </Button>
                 ) : (
                   <Button
@@ -425,7 +430,12 @@ const BorrowLoanRolloverCard: React.FC<Props> = ({
               }`}
             />
             <ModalCloseButton />
-            <Offers nft={nft} offers={offers} onOfferSelect={onSelectOffer} />
+            <Offers
+              actionLabel="Borrow"
+              nft={nft}
+              offers={offers}
+              onOfferSelect={onSelectOffer}
+            />
           </ModalContent>
         </Modal>
       )}
